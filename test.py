f952d274c3d722db8b91582a58954404939fc51e
Erin Ruby <erinruby@engr2-12-145-dhcp.int.colorado.edu>
2018-06-05 15:41:29 -0600

    added the delete one

--- test.py
+++ test.py
@@ -4,14 +4,13 @@ import pprint
 from pymongo import MongoClient
 client = pymongo.MongoClient("mongodb+srv://erinruby:colorado18@yame-project-6ex3z.mongodb.net/test?retryWrites=true")
 db = client.prototype
-collection = db.people
 
 person = {"name": "Lucile Berkeley Buchanan",
         "degree": "German",
-        "occupation": "Teaching",
-        "facts": datetime.datetime.utcnow()}
-people = db.people
-person_id = people.insert_one(person).inserted_id
-pprint.pprint(people.find_one())
+        "occupation": "Teaching",}
+
+# person_id = db.people.insert_one(person).inserted_id
+db.people.delete_one(person)
+# pprint.pprint(people.find_one())
 # print (db.collection_names(include_system_collections=False))
 # print (post_id)
