353a6774c763baf8134ae072d1275f3771efa905
Erin Ruby <erinruby@engr2-12-145-dhcp.int.colorado.edu>
2018-06-06 15:08:05 -0600

    photo name is the 4th argument

--- db/add_person.py
+++ db/add_person.py
@@ -11,10 +11,12 @@ def main():
     name = sys.argv[1]
     degree = sys.argv[2]
     occupation = sys.argv[3]
+    photo = sys.argv[4]
 
     fs=gridfs.GridFS(db)
     # print (fs.list())
-    im = fs.put(open('/Users/erinruby/Documents/Summer_2018/YAME/db/download.jpg', 'rb'), filename='download.jpg')
+    im = fs.put(open('/Users/erinruby/Documents/Summer_2018/YAME/db/{0}'.format(photo), 'rb'), filename='{0}'.format(photo)) #ERINS COMP
+    # im = fs.put(open('/YAME/form_ui/uploads/{0}'.format(photo), 'rb'), filename='{0}'.format(photo)) #COURTNEYS COMP
 
 
     person = {"name": name,
@@ -23,19 +25,19 @@ def main():
             "photo": im}
             # "photo": os.system("mongofiles --uri 'mongodb+srv://erinruby:colorado18@yame-project-6ex3z.mongodb.net/test' put 'download.jpg'")}
 
-    # if (db.people.insert_one(person).inserted_id != 0):
-        # print (name, "successfully added!")
+    if (db.people.insert_one(person).inserted_id != 0):
+        print (name, "successfully added!")
 
     pup = db.fs.files.find({'_id':im})
     for att in pup:
-        photo = att['filename']
+        p = att['filename']
         # for i in att:
         #     print (i)
         #     if (i == "filename"):
         #         photo = i.value()
         #         print(photo)
     # print (pup)
-    os.system("open {0}".format(photo))
+    os.system("open {0}".format(p))
 
 if __name__ == '__main__':
     main()
