69a6f55d3955a44a59e44dd5c431feabd301362a
Courtney Solano <courtsolano@gmail.com>
2018-06-06 16:27:07 -0600

    Dropdown menu is for different schools

--- form_ui/new_entry_form.py
+++ form_ui/new_entry_form.py
@@ -29,7 +29,7 @@ def hello():
         name=request.form['name'].replace(' ', '+')+' ' # key value pairs
         degree=request.form['degree'].replace(' ', '+')+' '
         occupation=request.form['occupation'].replace(' ', '+')+' '
-        number=request.form['number']+' '
+        school=request.form['school']+' '
         file = request.files['image']
         if file:
             f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
