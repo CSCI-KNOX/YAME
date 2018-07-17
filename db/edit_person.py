b0cf66a0773ca22fedf2fca7372cb9e9cc881aa0
Courtney Solano <courtsolano@gmail.com>
2018-07-17 13:34:21 -0600

    Edit page works again yay

--- db/edit_person.py
+++ db/edit_person.py
@@ -4,13 +4,12 @@ import gridfs
 import os
 
 
-def editOne(id, name, degree, school, ex, year, occupation, facts, photo, alt_txt, icon, hidden): # school, year, occupation, facts, filename, hidden
+def editOne(id, name, degree, school, ex, year, occupation, facts, photo, alt_txt, hidden): # school, year, occupation, facts, filename, hidden
 	client = pymongo.MongoClient("mongodb+srv://erinruby:colorado18@yame-project-6ex3z.mongodb.net/test?retryWrites=true") #ERIN's LOGIN
 	db = client.prototype #name of the db
 	col = client.people #name of the collection
 	fs = gridfs.GridFS(db)
 	im = fs.put(open('../form_ui/static/imj/{0}'.format(photo), 'rb'), filename='{0}'.format(photo)) #store the photo in the db
-	ic = fs.put(open('../form_ui/static/imj/{0}'.format(photo), 'rb'), filename='{0}'.format(icon)) #store the photo in the db
 
 	person = {"name": name,
 			"degree": degree,
@@ -20,9 +19,7 @@ def editOne(id, name, degree, school, ex, year, occupation, facts, photo, alt_tx
 			"occupation": occupation,
 			"facts": facts,
 			"image": im,
-			"icon":ic,
-			"icon_name": icon,
 			"image_name": photo,
 			"alt_txt": alt_txt,
 			"hidden": hidden}
-	db.people.update_one({"_id": id}, { "$set": {person}})
+	db.people.update_one({"_id": id}, { "$set": person })
