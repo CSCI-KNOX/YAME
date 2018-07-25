eb8cc3f663ecf860dd93ec1a2f24cf833908a947
Courtney Solano <courtsolano@gmail.com>
2018-07-25 14:20:56 -0600

    Finally people are showing up

--- form_ui/static/js/tabs.js
+++ form_ui/static/js/tabs.js
@@ -1,5 +1,6 @@
 function openCity(cityName, elmnt) {
     var i, tabcontent, tablinks;
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -9,8 +10,10 @@ function openCity(cityName, elmnt) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "black";
     }
+
     // Show the specific tab content
     document.getElementById(cityName).style.display = "block";
+
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = '#777';
 }
